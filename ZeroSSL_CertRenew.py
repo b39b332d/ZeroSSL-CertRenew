ed356dfb6b7f956ee334eece8929a7a3ded1165c
b39b332d <42030986+b39b332d@users.noreply.github.com>
2024-05-27 15:14:34 +0800

    follow zerossl api update
    change to http get method; add some configuration

--- ZeroSSL_CertRenew.py
+++ ZeroSSL_CertRenew.py
@@ -1,3 +1,4 @@
+#!/bin/python3
 import requests
 import subprocess
 import json
@@ -6,6 +7,19 @@ from pathlib import Path
 import time
 import shutil
 import glob
+import pwd
+import os
+
+#
+#   nginx
+#	ssl_certificate /etc/nginx/ssl/{your domain name}_cert.pem;
+#	ssl_certificate_key /etc/nginx/ssl/{your domain name}_key.pem;
+#
+#   apache
+#   SSLCertificateFile	/etc/apache2/ssl/{your domain name}_cert.pem
+#   SSLCertificateKeyFile /etc/apache2/ssl/{your domain name}_key.pem
+#
+
 
 # Suppress https warning (Burp)
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
@@ -16,16 +30,16 @@ class SSLCertReNew(object):
         self.url = 'https://api.zerossl.com'
         #self.proxies = { 'http' : 'http://127.0.0.1:8080', 'https' : 'http://127.0.0.1:8080' } # for testing purposes with Burp
         self.proxies = None
-        self.apiKey = 'API_KEY_HERE' #https://app.zerossl.com/developer
-        self.certificateDomain = 'domain.com'
+        self.apiKey = 'your api key' #https://app.zerossl.com/developer
+        self.certificateDomain = 'your domain name'
+        self.install_loc = '/etc/nginx/ssl/'
+        self.web_root = '/var/www/html/'
         self.csr = self.createCsr()
         #run steps
         self.InitialRequest()
         self.VerificationMethods()
-        if(self.status == 0):
-            time.sleep(10)
-        else:
-            self.DownloadAndSave()
+        self.VerificationStatus()
+        self.DownloadAndSave()
             
 
     def createCsr(self):
@@ -71,6 +85,7 @@ subjectAltName = DNS: www.{self.certificateDomain}, DNS: {self.certificateDomain
                                         'certificate_csr': self.csr }
                                 )
         result = json.loads(response.text)
+        print(result)
         self.certHash = result['id']
         #url from json
         self.HttpsUrl = result['validation']['other_methods'][f'{self.certificateDomain}']['file_validation_url_https']
@@ -79,11 +94,11 @@ subjectAltName = DNS: www.{self.certificateDomain}, DNS: {self.certificateDomain
         self.dirTwo = self.HttpsUrl.split('/')[-2]
         self.fileName = self.HttpsUrl.split('/')[-1]
         #create directories
-        Path(f'/var/www/{self.certificateDomain}/{self.dirOne}/{self.dirTwo}').mkdir(parents=True, exist_ok=True)
+        Path(f'/var/www/html/{self.dirOne}/{self.dirTwo}').mkdir(parents=True, exist_ok=True)
         #save file
         #convert array into string with newline
         string = '\n'.join(result['validation']['other_methods'][f'{self.certificateDomain}']['file_validation_content'])
-        f = open(f'/var/www/{self.certificateDomain}/{self.dirOne}/{self.dirTwo}/{self.fileName}', 'w')
+        f = open(f'/var/www/html/{self.dirOne}/{self.dirTwo}/{self.fileName}', 'w')
         f.write(string)
         f.close()
 
@@ -92,9 +107,15 @@ subjectAltName = DNS: www.{self.certificateDomain}, DNS: {self.certificateDomain
                                  proxies=self.proxies, data={ 'validation_method': 'HTTPS_CSR_HASH' })
 
     def VerificationStatus(self):
-        response = requests.post(self.url+f'/certificates/{self.certHash}/status?access_key={self.apiKey}', proxies=self.proxies)
-        result = json.loads(response.text)
-        self.status = result['validation_completed']
+        for i in range(10):
+            time.sleep(1)
+            response = requests.get(self.url+f'/certificates/{self.certHash}/status?access_key={self.apiKey}', proxies=self.proxies)
+            result = json.loads(response.text)
+            if 'validation_completed' not in result:
+                raise Exception(str(result))
+            self.status = result['validation_completed']
+            if self.status==1:
+                break
 
     def DownloadAndSave(self):
         response = requests.get(self.url+f'/certificates/{self.certHash}/download/return?access_key={self.apiKey}',verify = False)
@@ -103,19 +124,23 @@ subjectAltName = DNS: www.{self.certificateDomain}, DNS: {self.certificateDomain
         ca_bundle = result['ca_bundle.crt']
         cert = result['certificate.crt']
         
-        f = open('/etc/apache2/ssl/{self.certificateDomain}_cert.pem', 'w+')
+        id=pwd.getpwnam('www-data')
+
+        f = open(f'{self.install_loc}{self.certificateDomain}_cert.pem', 'w+')
         f.write(cert)
         f.close()
-        
-        f = open('/etc/apache2/ssl/{self.certificateDomain}_ca.pem', 'w+')
+        os.chown(f'{self.install_loc}{self.certificateDomain}_cert.pem',id.pw_uid,id.pw_gid)
+        f = open(f'{self.install_loc}{self.certificateDomain}_ca.pem', 'w+')
         f.write(ca_bundle)
         f.close()
+        os.chown(f'{self.install_loc}{self.certificateDomain}_ca.pem',id.pw_uid,id.pw_gid)
 
         #move private key
-        shutil.move(f'{self.certificateDomain}_key.pem', f'/etc/apache2/ssl/{self.certificateDomain}_key.pem')
+        shutil.move(f'{self.certificateDomain}_key.pem', f'{self.install_loc}{self.certificateDomain}_key.pem')
+        os.chown(f'{self.install_loc}{self.certificateDomain}_key.pem',id.pw_uid,id.pw_gid)
 
         #delete files in /var/www/site/.wellknown/pki-verification
-        files = glob.glob(f'/var/www/{self.certificateDomain}/{self.dirOne}/{self.dirTwo}/*')
+        files = glob.glob(f'{self.web_root}{self.dirOne}/{self.dirTwo}/*')
         for f in files:
             os.remove(f)
 
